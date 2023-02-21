c0c428587f36f90d5644a67ad180d510144d73bf
Vikas Chauhan <vikaschauhanmail@gmail.com>
2023-02-21 21:50:05 +0530

    Collection Basics Cheat Sheet For Beginners

---
+++ src/collectionPack/Driver.java
@@ -0,0 +1,45 @@
+package collectionPack;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.List;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	List<String> abc=new 	ArrayList<String>() ;
+		//abc.add("Mouli");
+		//abc.add("Vikas");
+		abc =  Arrays.asList(new String[] {"Mouli" , "Vikas", "ABC"});
+		//Comparable
+		int[] arr = {2,4,6};
+		Collections.sort(abc);
+		System.out.println(abc);
+		
+		//to iterate 
+		for(String str: abc)
+			System.out.println(str);
+		//second way
+		Iterator<String> itr = abc.iterator();
+		while(itr.hasNext()) {
+            // if(itr.next().equals("ABC"))
+			//itr.remove();
+              //// no  ConcurrentModificationexception
+             
+			System.out.println(itr.next());
+		//itr.remove();
+		}
+			
+		//third way 
+		Enumeration e =Collections.enumeration(abc);
+		while(e.hasMoreElements())
+			System.out.println(e.nextElement());
+		
+
+	}
+
+}
---
+++ src/collectionPack/StoreData.java
@@ -0,0 +1,12 @@
+package collectionPack;
+
+public class StoreData {
+int data;
+String category;
+public StoreData(int data , String category) {
+	this.data = data;
+	this.category = category;
+}
+
+
+}
